505c1db66e5b4564565841dcc59ff065f4f713ed
sumeddha <2100031034@kluniversity.in>
2024-06-08 14:37:40 +0530

    commit

--- jpmc/src/main/java/com/example/jpmc/controller/UserController.java
+++ jpmc/src/main/java/com/example/jpmc/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
 	@Autowired
 	private UserService uservice;
 	
-	@PostMapping("/register")
+	@PostMapping("/registration")
 	public User submitUser(@RequestBody User user) {
 		
 		return uservice.saveUser(user);
